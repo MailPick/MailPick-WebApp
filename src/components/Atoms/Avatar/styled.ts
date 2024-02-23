961f2b486d3260b8e3332467b8915b9e559a72bc
hyeonu <hyeonu030334@gmail.com>
2024-02-24 02:08:55 +0900

    style: Avatar Component 스타일수정

--- src/components/Atoms/Avatar/styled.ts
+++ src/components/Atoms/Avatar/styled.ts
@@ -1,17 +1,25 @@
 import tw from "twin.macro";
 
-export const Box = tw.div`
+export const EmailIconBox = tw.div`
   flex 
   justify-center 
   items-center 
   rounded-full 
-  w-10
-  h-10
+  w-8
+  h-8
   p-1
   border-none
   overflow-hidden
-  bg-gray-200
+  bg-gray-100
 `;
+
+export const ImgBox = tw.div`
+  w-8
+  h-8
+  overflow-hidden
+  rounded-full
+  border-none
+`
 
 export const Img = tw.img`
   w-full
