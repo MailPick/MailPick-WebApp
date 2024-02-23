b423c4159a6bcb718f7fd2afb353733b6340fb0d
hyeonu <hyeonu030334@gmail.com>
2024-02-23 18:43:04 +0900

    feat: Avatar 컴포넌트 구현

--- src/components/Atoms/Avatar/Avatar.tsx
+++ src/components/Atoms/Avatar/Avatar.tsx
@@ -1,9 +1,26 @@
+import Icon from "../Icon/Icon";
+import { Box, Img } from "./styled";
+import useEmailIcon from "@/hooks/useEmailIcon";
+interface AvatarProp {
+  imageUrl?: string;
+  email:string
+  alt?:string
+}
 
-
-const Avatar = () => {
+//이미지 url이 있으면 이미지보여주고 이미지 url이 없으면 email로고보여줌
+const Avatar:React.FC<AvatarProp> = ({imageUrl,email, alt}) => {
+  const iconId = useEmailIcon(email)
   return(
-    <div></div>
+    <Box>
+      {
+        imageUrl === "" ? (
+          <Icon id={iconId} width="30" height="30"/>
+        ) : (
+          <Img src={imageUrl} alt={alt}/>
+        )
+      }
+    </Box>
   )
 }
 
-export default Avatar;
+export default Avatar; 
