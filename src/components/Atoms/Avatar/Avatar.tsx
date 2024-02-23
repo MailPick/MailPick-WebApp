07e069755a3d91978c03031d9a918984aafa90bf
hyeonu <hyeonu030334@gmail.com>
2024-02-24 02:08:20 +0900

    fix: Avatar Icon 로직수정

--- src/components/Atoms/Avatar/Avatar.tsx
+++ src/components/Atoms/Avatar/Avatar.tsx
@@ -1,25 +1,36 @@
 import Icon from "../Icon/Icon";
-import { Box, Img } from "./styled";
+import { EmailIconBox, Img, ImgBox } from "./styled";
 import useEmailIcon from "@/hooks/useEmailIcon";
+
 interface AvatarProp {
-  imageUrl?: string;
+  emailIconUrl?: string;
   email:string
-  alt?:string
 }
 
-//이미지 url이 있으면 이미지보여주고 이미지 url이 없으면 email로고보여줌
-const Avatar:React.FC<AvatarProp> = ({imageUrl,email, alt}) => {
-  const iconId = useEmailIcon(email)
+
+/**
+ * 
+ */
+const Avatar:React.FC<AvatarProp> = ({
+  emailIconUrl,
+  email
+  }) => {
+  const emailIconId = useEmailIcon(email)
   return(
-    <Box>
-      {
-        imageUrl === "" ? (
-          <Icon id={iconId} width="30" height="30"/>
-        ) : (
-          <Img src={imageUrl} alt={alt}/>
-        )
-      }
-    </Box>
+    <>
+    {
+      emailIconUrl ? (
+        <ImgBox>
+          <Img src={emailIconUrl}/>
+        </ImgBox>
+        
+      ):(
+        <EmailIconBox>
+          <Icon id={emailIconId} width="28" height="28"/>
+        </EmailIconBox>
+      )
+    }
+    </>
   )
 }
 
