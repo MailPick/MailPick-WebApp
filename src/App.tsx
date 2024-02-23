367864c75e007b7cb84548bf6c8975b841a322a0
hyeonu <hyeonu030334@gmail.com>
2024-02-23 18:44:39 +0900

    docs: App.tsx Avatar 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
+import Avatar from './components/Atoms/Avatar';
 import Button from './components/Atoms/Button'
 import Label from './components/Atoms/Label'
 
+const exampleProfile = "https://i.ibb.co/KVVtbHS/2024-02-23-11-37-52.png";
 function App() {
   return (
     <>
@@ -11,6 +13,8 @@ function App() {
       <Button variant={"warning"} size={"md"}>등록하기</Button>
       <Label fontSize={"40px"} fontBold >안녕</Label>
       <Label fontSize={"100px"} fontBold>안녕</Label>
+      <Avatar imageUrl={exampleProfile} email={"hrewrw@naver.com"}/>
+      <Avatar imageUrl="" email={""}/>
     </>
   )
 }
