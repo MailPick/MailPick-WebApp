3fcad263108a47a98f66a00fabc569c44453a40e
hyeonu <hyeonu030334@gmail.com>
2024-02-26 16:24:01 +0900

    docs: App.tsx 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
-import Avatar from './components/Atoms/Avatar';
 import Button from './components/Atoms/Button'
 import Label from './components/Atoms/Label'
-
-const exampleProfile = "https://i.ibb.co/KVVtbHS/2024-02-23-11-37-52.png";
+import {Text} from './components/Atoms/Text/Text';
+import {Text as Title} from './components/Atoms/Text/Text';
+import Vertical from './components/Atoms/VerticalBar/VerticalBar';
 function App() {
   return (
     <>
@@ -13,8 +13,10 @@ function App() {
       <Button variant={"warning"} size={"md"}>등록하기</Button>
       <Label fontSize={"40px"} fontBold >안녕</Label>
       <Label fontSize={"100px"} fontBold>안녕</Label>
-      <Avatar imageUrl={exampleProfile} email={"hrewrw@naver.com"}/>
-      <Avatar imageUrl="" email={""}/>
+      <Text fontSize="40px" fontBold>안녕</Text>
+      <Text fontSize="50px" fontBold>안녕하세요</Text>
+      <Title fontSize={50} fontBold>제목</Title>
+      <Vertical color="#f83939"/>
     </>
   )
 }
