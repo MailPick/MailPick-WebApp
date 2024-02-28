3949b661c7619b4f1b20ad25c3bbe968b655261e
hyeonu <hyeonu030334@gmail.com>
2024-02-28 17:36:12 +0900

    test: EmailCard Test 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import './App.css'
-import Button from './components/Atoms/Button'
-import Label from './components/Atoms/Label'
-import {Text} from './components/Atoms/Text/Text';
-import {Text as Title} from './components/Atoms/Text/Text';
-import Vertical from './components/Atoms/VerticalBar/VerticalBar';
+import EmailCard from './components/Blocks/EmailCard/EmailCard';
 function App() {
   return (
     <>
-      <Button variant={"danger"} size={"md"}>등록하기</Button>
-      <Button variant={"default"} size={"md"}>등록하기</Button>
-      <Button variant={"success"} size={"md"}>등록하기</Button>
-      <Button variant={"warning"} size={"md"}>등록하기</Button>
-      <Label fontSize={"40px"} fontBold >안녕</Label>
-      <Label fontSize={"100px"} fontBold>안녕</Label>
-      <Text fontSize="40px" fontBold>안녕</Text>
-      <Text fontSize="50px" fontBold>안녕하세요</Text>
-      <Title fontSize={50} fontBold>제목</Title>
-      <Vertical color="#f83939"/>
+      <EmailCard 
+        name="PHW" 
+        title="[중요] 2월 5일 개발 프로젝트 회의 안내" 
+        content={`안녕하세요, 은서님, 우선, 귀하의 노고에 진심으로 감사드립니다.
+        저희 메일픽의 중요한 다음 단계를 논의하기 위해 개발 프로젝트 회의를 개최하려고 합니다. 회의에 관한 세부 사항은 아래와 같습니다.
+        `}
+        verticalColor="#ff8484"
+        email="phw@gmail.com"
+        time="12:34pm"
+      />
     </>
   )
 }
