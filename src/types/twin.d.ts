5c769f6cb809ee66ae9efa4186a0cc991566e748
hyeonu <hyeonu030334@gmail.com>
2024-02-28 17:33:45 +0900

    chore: twin macro type 수정

--- src/types/twin.d.ts
+++ src/types/twin.d.ts
@@ -1,11 +1,30 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import 'twin.macro'
+import styledImport, { CSSProp, css as cssImport } from 'styled-components'
 
-import { css } from 'styled-components'
-import {styled} from 'styled-components'
+declare module 'twin.macro' {
+  // The styled and css imports
+  const styled: typeof styledImport
+  const css: typeof cssImport
+}
 
-declare module 'twin.macro'{
-  const styled: typeof styled;
-  const css: typeof css
+declare module 'react' {
+  // The css prop
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    css?: CSSProp
+    tw?: string
+  }
+  // The inline svg css prop
+  interface SVGProps<T> extends SVGProps<SVGSVGElement> {
+    css?: CSSProp
+    tw?: string
+  }
+}
+
+// The 'as' prop on styled components
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
+      as?: string | Element
+    }
+  }
 }
