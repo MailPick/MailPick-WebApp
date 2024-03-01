1dcb1863cd0e9532317f00a9d2a5bfab2a20882a
hyeonu <hyeonu030334@gmail.com>
2024-03-01 23:46:43 +0900

    refactor: Icon컴포넌트 수정
    props 재정의 및 width height 기본값 24로 설정

--- src/components/Atoms/Icon/Icon.tsx
+++ src/components/Atoms/Icon/Icon.tsx
@@ -1,7 +1,4 @@
 //svg sprilt 방식
-/**Todo
- * 마우스 올려놓을때 배경색 blue div로 있어야함
- */
 export const IconId = {
   BiInbox:"biInbox",
   File:"file",
@@ -17,23 +14,23 @@ export const IconId = {
   Daum:"daum"
 } as const
 
-type IconId = typeof IconId[keyof typeof IconId]
+export type IconId = typeof IconId[keyof typeof IconId]
 
-interface SvgIconProps {
+interface SvgIconProps extends React.SVGProps<SVGSVGElement>{
   id : IconId;
-  width : string;
-  height : string;
+  width? : string;
+  height? : string;
 }
 
 const Icon = ({
   id,
-  width,
-  height,
+  width = "24px",
+  height = "24px",
   ...props
 }:SvgIconProps) => {
   return(
-    <svg width={width} height={height} {...props}>
-      <use href={`#${id}`} />
+    <svg width={width} height={height} fill="currentColor"{...props}>
+      <use href={`#${id}`}/>
     </svg>
   )
 }
