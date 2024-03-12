a917c7c260ecd0ece7cb9cec3095bafb93e7e5fa
hyeonu <hyeonu030334@gmail.com>
2024-03-12 17:17:01 +0900

    refactor: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import './App.css'
-import EmailCard from './components/Blocks/EmailCard/EmailCard';
+import tw from 'twin.macro'
 function App() {
+
   return (
-    <>
-      <EmailCard 
-        name="PHW" 
-        title="[중요] 2월 5일 개발 프로젝트 회의 안내" 
-        content={`안녕하세요, 은서님, 우선, 귀하의 노고에 진심으로 감사드립니다.
-        저희 메일픽의 중요한 다음 단계를 논의하기 위해 개발 프로젝트 회의를 개최하려고 합니다. 회의에 관한 세부 사항은 아래와 같습니다.
-        `}
-        verticalColor="#ff8484"
-        email="phw@gmail.com"
-        time="12:34pm"
-      />
-    </>
+    <Container>
+      <div>
+
+      </div>
+    </Container>
   )
 }
+const Container = tw.div`flex flex-row w-full min-h-screen`
+
 
 export default App;
