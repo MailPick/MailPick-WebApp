7fa85abfa03a2fb3c23dd5a05c1d714a059e48b9
hyeonu <hyeonu030334@gmail.com>
2024-03-17 11:19:38 +0900

    refector: 코드 변경

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,13 @@
 import Sidebar from "@/components/Organism/Sidebar";
-import EmailListCard from "@/components/Organism/EmailListCard";
 import tw from "twin.macro";
-import { useState } from "react";
+import ViewEmailList from "@/components/Organism/ViewEmailList";
 const Main = () => {
-  const [selectedSection, setSelectedSection] = useState("inbox");
   return(
     <Container>
-      <Sidebar setSelectedSection={setSelectedSection}/>
-      <EmailListCard selectedSection={selectedSection}/>
+      <Sidebar/>
+      <ViewEmailList/>
     </Container>
   )
 }
-const Container = tw.div`flex flex-row w-full min-h-screen`
+const Container = tw.div`flex w-full min-h-screen `
 export default Main;
