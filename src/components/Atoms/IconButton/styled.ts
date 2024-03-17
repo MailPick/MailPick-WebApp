2e4dac458d4584bcd1dbe88f26208d199d5d982c
hyeonu <hyeonu030334@gmail.com>
2024-03-17 11:03:48 +0900

    refector: width 동적 변경로직 추가

--- src/components/Atoms/IconButton/styled.ts
+++ src/components/Atoms/IconButton/styled.ts
@@ -1,6 +1,8 @@
 import tw, { styled } from 'twin.macro';
 
 interface StyledIconButtonProps {
+  width?:string;
+  height?:string;
   $isActive?: boolean;
 }
 
@@ -9,18 +11,17 @@ export const StyledIconButton = styled.div<StyledIconButtonProps>`
     flex 
     items-center 
     justify-center 
-    w-[36px] h-[36px] 
     rounded-md 
     select-none
-    `
-  }
-  
+  `}
+  width: ${({ width }) => width || '36px'};
+  height: ${({ height }) => height || '36px'};
   &:hover {
-    ${tw`bg-gray-300/50`}
+    /* ${tw`bg-gray-300/50`} */
   }
   &:active {
     ${tw`text-blue-500 bg-blue-500/20`}
   }
   
-  ${({ $isActive }) => $isActive?tw`text-blue-500 bg-blue-500/20` : tw`text-gray-400`}
+  ${({ $isActive }) => $isActive ? tw`text-blue-500 bg-blue-500/20` : tw`text-gray-400`}
 `;
