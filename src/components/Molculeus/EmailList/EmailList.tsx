909c8b22a52b3ca0a6115004b4453217b5323fdf
hyeonu <hyeonu030334@gmail.com>
2024-03-17 11:04:37 +0900

    feat: EmailList 컴포넌트 구현
    이메일을 보여주는 화면을 재사용하기위한 EmailList컴포넌트

---
+++ src/components/Molculeus/EmailList/EmailList.tsx
@@ -0,0 +1,59 @@
+import { EmailType } from "@/types/data.type";
+import { useAccountStore } from "@/store/accountStore";
+import EmailCard from "../EmailCard";
+import { groupEmailsByDate,formatDateForEmailCard } from "@/services/dateHelper";
+import { DateGroupTitle } from "./styled";
+import { useActiveEmailStore } from "@/store/activeEmailStore";
+interface Props{
+  emails: EmailType[];
+  userInput: string;
+}
+
+const EmailList = ({emails,userInput}:Props) => {
+  const {activeEmail,setActiveEmail} = useActiveEmailStore();
+  const filteredEmails = emails?.filter((email: EmailType) => {
+    const searchQuery = userInput.toLowerCase();
+    return  email.from.toLowerCase().includes(searchQuery) ||
+            email.subject.toLowerCase().includes(searchQuery) ||
+            email.body.plain.toLowerCase().includes(searchQuery);
+  });
+  const accounts = useAccountStore((state) => state.account);
+  const findIdentifyColor = (emailTo:string) => {
+    const accountMatch = accounts.find((account) => account.account === emailTo);
+    return accountMatch ? accountMatch.identifyColor : undefined;
+  };
+  const emailsGroupedByDate = groupEmailsByDate(filteredEmails);
+
+  console.log("selectEmail",activeEmail)
+  return(
+    <>
+      {Object.entries(emailsGroupedByDate).map(([dateGroup, emails]) => (
+        <div key={dateGroup}>
+          <DateGroupTitle fontSize={"12px"} fontWeight={"semibold"}>
+            {dateGroup}
+          </DateGroupTitle>
+          {emails.map((email: EmailType) => {
+            const identifyColor = findIdentifyColor(email.to);
+            const formattedDate = formatDateForEmailCard(email.date);
+            return (
+              <div key={email.uid}>
+                <EmailCard 
+                  from={email.from}
+                  subject={email.subject}
+                  plain={email.body.plain}
+                  date={formattedDate}
+                  email={email.from}
+                  verticalColor={identifyColor || 'defaultColor'}
+                  onClick={() => setActiveEmail(email)}
+                  isActive={email.uid === activeEmail?.uid}
+                />
+              </div>
+            );
+          })}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default EmailList;
---
+++ src/components/Molculeus/EmailList/styled.ts
@@ -0,0 +1,9 @@
+import tw from "twin.macro";
+import { StyledText } from "@/components/Atoms/Text/styled";
+
+export const DateGroupTitle =tw(StyledText)`
+  px-[10px]
+  py-[4px]
+  text-gray-400
+  select-none
+`
