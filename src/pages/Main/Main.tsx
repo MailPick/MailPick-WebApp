c9dddebf1250ed264e08ec8140d2cb34c9a5091c
hyeonu <hyeonu030334@gmail.com>
2024-03-27 21:38:10 +0900

    refector: Main.tsx SideContent코드 추가, 스타일변경

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,29 +4,59 @@ import ViewEmailList from "@/components/Organism/ViewEmailList";
 import ViewEmailDetail from "@/components/Organism/ViewEmailDetail";
 import { useSelectSectionStore, useSelectTopButtonStore } from "@/store/selectSectionStore";
 import ViewCalendar from "@/components/Organism/ViewCalendar/ViewCalendar";
+import EmailScheduleSummary from "@/components/Molculeus/EmailScheduleSummary";
+import SideCalendar from "@/components/Molculeus/SideCalendar/SideCalendar";
 const Main = () => {
   const {selectSection} = useSelectSectionStore();
   const {selectTopButton} = useSelectTopButtonStore();
   console.log("main select",selectSection);
   return(
-    <Container>
+    <MainContainer>
       <Sidebar/>
         { selectTopButton != "calendar" ? (
-          <MainContent>
-            <ViewEmailList/>
-            <ViewEmailDetail/>
-          </MainContent>
+          <>
+            <MainContent>
+              <ViewEmailList/>
+              <ViewEmailDetail/>
+            </MainContent>
+            <SideContent>
+              <EmailScheduleSummary/>
+              <SideCalendar/>
+            </SideContent>
+          </>
           ): (
           <>
             <ViewCalendar/>
           </>
           )
         }
-    </Container>
+    </MainContainer>
   )
 }
-const Container = tw.div`flex w-full overflow-x-auto min-h-screen bg-gray-main static`
+const MainContainer = tw.div`
+  flex 
+  flex-row
+  bg-gray-main
+  min-w-[1600px]
+  `
 const MainContent = styled.div`
-  ${tw`flex  relative w-full z-0`}
+  ${tw`
+  flex 
+  flex-row
+  grow
+`}
 `
+const SideContent = tw.div`
+  flex 
+  flex-col 
+  gap-[20px]
+  min-w-[336px]
+  max-w-[336px]
+  mt-[28px]
+  pr-[20px]
+  overflow-y-hidden
+  h-[calc(100vh-62px)]
+  rounded-lg
+  overflow-x-hidden
+  `
 export default Main;
